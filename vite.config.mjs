89f673fc597b9c7e7c9c111bc078e3d8e8ffd996
Mario Oliveira <m.eos2009@hotmail.com>
2026-01-30 16:30:53 +0000

    Renomear Relatório para Relatório de Cotas

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Gestor de Cotas - UD Fafe A60',
+        short_name: 'UD Fafe A60',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0A2A43',
+        theme_color: '#0A2A43',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
     port: 5173,
   },
   build: {
     outDir: 'dist',
-  }
-})
+  },
+});
