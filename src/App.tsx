9e0acd79620cc1a1b4320bb9ceca17196bf4686d
Mario Oliveira <m.eos2009@hotmail.com>
2026-03-12 16:20:15 +0000

    Remover alerta do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-console.log(">>> ESTATISTICAS CORRETA <<<");
-alert("ESTATISTICAS CORRETA");
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import RequireAuth from "./lib/RequireAuth";
