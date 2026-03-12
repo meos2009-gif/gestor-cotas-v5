ded4bec6f80c8b26755015bdb9066b218ca09c3d
Mario Oliveira <m.eos2009@hotmail.com>
2026-03-12 15:55:14 +0000

    TESTE MARIO

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 <Route path="/convocatoria/:gameId" element={<ConvocatoriaNova />} />
+<Route path="/convocatoria/:gameId" element={<div style={{padding: 20}}>TESTE MARIO</div>} />
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
