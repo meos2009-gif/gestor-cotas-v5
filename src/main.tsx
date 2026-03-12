074c8ffdeef68290165e75c118998d119513e49f
Mario Oliveira <m.eos2009@hotmail.com>
2026-03-12 16:40:07 +0000

    Corrigir main.tsx e remover rotas duplicadas

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import ConvocatoriaNova from "./pages/ConvocatoriaNova";
-<Route path="/convocatoria/:gameId" element={<ConvocatoriaNova />} />
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/convocatoria/:gameId" element={<Convocatoria />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
