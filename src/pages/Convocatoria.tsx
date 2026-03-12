708474db736b6f7adfb2d00e9bb5892047196d7b
Mario Oliveira <m.eos2009@hotmail.com>
2026-03-12 08:44:12 +0000

    Fix supabase import

--- src/pages/Convocatoria.tsx
+++ src/pages/Convocatoria.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import supabase from "../supabaseClient";
-
+import { supabase } from "../supabaseClient";
 type Member = {
   id: string;
   name: string;
